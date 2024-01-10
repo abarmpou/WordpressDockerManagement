fa5adc26292de54fef147378f739310ffb0a8201
Angelos Barmpoutis <131296747+abarmpou@users.noreply.github.com>
2024-01-10 10:38:08 -0500

    Create create_site.sh

---
+++ create_site.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+# Process command-line options
+while getopts "u:" opt; do
+    case $opt in
+        u) url_option="$OPTARG";;
+        *) echo "Invalid option"; exit 1;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
+# Check if site_name is provided as an argument
+if [ -z "$1" ]; then
+    echo "Please provide the site_name as an argument."
+    echo "Example: create_site.sh example
+    exit 1
+fi
+
+site_name="$1"
+
+# Check if url_option is not empty
+if [ -n "$url_option" ]; then
+    echo "URL: $url_option"
+else
+    url_option="https://$site_name"
+    echo "URL: $url_option"
+fi
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Installing docker..."
+    apt-get update
+    apt-get install docker
+    apt install docker.io
+    docker --version
+fi
+
+# Define the image name
+image_name="mariadb"
+
+# Check if the Docker image exists
+if docker images "$image_name" | grep "$image_name" >/dev/null; then
+    echo "The Docker image '$image_name' exists."
+else
+    echo "The Docker image '$image_name' does not exist. Pulling image..."
+    docker pull "$image_name"
+fi
+
+# Define the image name
+image_name="wordpress"
+
+# Check if the Docker image exists
+if docker images "$image_name" | grep "$image_name" >/dev/null; then
+    echo "The Docker image '$image_name' exists."
+else
+    echo "The Docker image '$image_name' does not exist. Pulling image..."  
+    docker pull "$image_name:latest"
+fi
+
+# Check if a local folder with the site_name exists
+if [ -d "$site_name" ]; then
+    echo "A local folder with the name '$site_name' exists. Please remove it."
+    exit 1
+else
+    echo "Creating local folder '$site_name'..."
+    mkdir -p "$site_name"/{html,database}
+fi
+
+port=8101
+port_found=false
+
+while [ "$port_found" = false ] && [ "$port" -lt 8200 ]; do
+    if docker ps | grep ":$port" >/dev/null; then
+        port=$((port + 1))
+    else
+        port_found=true
+    fi
+done
+
+if [ "$port_found" = false ]; then
+    echo "No available ports found in the range."
+else
+    echo "Available port: $port"
+fi
+
+current_directory=$(pwd)
+
+
+docker run -e MYSQL_ROOT_PASSWORD=root-password -e MYSQL_USER="$site_name"_user -e MYSQL_PASSWORD=password -e MYSQL_DATABASE="$site_name"_db -v "$current_directory"/"$site_name"/database:/var/lib/mysql --name "$site_name"_db -d --restart unless-stopped mariadb
+
+container_ip=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' "$site_name"_db)
+echo "The IP address of the container ${site_name}_db is: $container_ip"
+
+docker run -e WORDPRESS_DB_USER="$site_name"_user -e WORDPRESS_DB_PASSWORD=password -e WORDPRESS_DB_NAME="$site_name"_db -p "$port":80 -v "$current_directory"/"$site_name"/html:/var/www/html --link "$site_name"_db:mysql --name "$site_name"_wp -d --restart unless-stopped wordpress:latest
+
+#wordpress:5.0.0 could be helpful for migrating an old site
+
+
+while [ ! -f "$site_name"/html/index.php ]; do
+    echo "Waiting for server to come alive..."
+    sleep 5
+done
+
+chown -R www-data:www-data "$site_name"/html
+
+# Extracting path after the domain
+path=$(echo "$url_option" | sed 's|^[^/]*//[^/]*\(.*\)|\1|')
+
+if [[ $path == /* ]]; then
+    path="${path#"/"}"  # Remove the leading /
+fi
+
+# Check if url_option is not empty
+if [ -n "$path" ]; then
+    if [[ ! $path == */ ]]; then
+        path="$path/"  # Add / at the end
+    fi
+    echo "WP installation at the path: $path"
+else
+    echo "WP installation at the root of the domain"
+    exit 0
+fi
+
+mkdir -p "$site_name"/html2/"$path"
+cp -r "$site_name"/html/* "$site_name"/html2/"$path"
+rm -r "$site_name"/html
+mv "$site_name"/html2 "$site_name"/html
+
+echo "RewriteEngine On" > "$site_name"/html/"$path".htaccess
+echo "RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]" >> "$site_name"/html/"$path".htaccess
+echo "RewriteBase /$path" >> "$site_name"/html/"$path".htaccess
+echo "RewriteRule ^index\.php$ - [L]" >> "$site_name"/html/"$path".htaccess
+echo "RewriteCond %{REQUEST_FILENAME} !-f" >> "$site_name"/html/"$path".htaccess
+echo "RewriteCond %{REQUEST_FILENAME} !-d" >> "$site_name"/html/"$path".htaccess
+echo "RewriteRule . /${path}index.php [L]" >> "$site_name"/html/"$path".htaccess
+
+chown -R www-data:www-data "$site_name"/html
+
+docker stop "$site_name"_wp
+docker start "$site_name"_wp
+
+
+echo "You are all set!"
+echo "Make sure you add to your Apache2 conf file the lines:"
+echo ""
+echo "RequestHeader set X-Forwarded-Proto \"https\""
+echo "RequestHeader set X-Forwarded-Port \"443\""
+echo "ProxyPass /projects/$site_name/ http://localhost:$port/projects/$site_name/"
+echo "ProxyPassReverse /projects/$site_name/ http://localhost:$port/projects/$site_name/"
+echo "ProxyPreserveHost On"
