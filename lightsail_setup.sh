435db92997f7da1b5c8579039ed36bfa2ef81455
Angelos Barmpoutis <131296747+abarmpou@users.noreply.github.com>
2025-01-24 15:07:00 -0500

    Create lightsail_setup.sh

---
+++ lightsail_setup.sh
@@ -0,0 +1,26 @@
+# Setup swap memory
+sudo fallocate -l 1G /swapfile  # Allocate 1GB of disk space
+sudo chmod 600 /swapfile       # Set the correct permissions
+sudo mkswap /swapfile          # Format the file as swap space
+sudo swapon /swapfile          # Enable the swap file
+/swapfile none swap sw 0 0    # Make it permanent
+
+# Install components
+sudo apt update
+sudo apt install mariadb-client
+sudp apt install apache2
+sudo apt install certbot python3-certbot-apache
+
+# Enable apache mods
+sudo a2enmod headers
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+sudo a2enmod proxy_balancer
+sudo a2enmod lbmethod_byrequests
+sudo a2enmod rewrite
+sudo a2enmod ssl
+sudo systemctl restart apache2
+
+
+
+
